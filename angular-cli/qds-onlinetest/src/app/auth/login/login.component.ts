730bebd43dd5aa8c67284b675f427618a055efa6
Xuong Huynh <xuong.huynh@quodisys.com>
2021-01-11 11:03:01 +0700

    Update Home page

--- angular-cli/qds-onlinetest/src/app/auth/login/login.component.ts
+++ angular-cli/qds-onlinetest/src/app/auth/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
 
 	ngOnInit() {
 		this.loginForm = this.formBuilder.group({
-            email: ['', [Validators.required, Validators.email]],
+            email: ['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$")]],
             password: ['', [Validators.required, Validators.minLength(6)]]
         })
 	}
