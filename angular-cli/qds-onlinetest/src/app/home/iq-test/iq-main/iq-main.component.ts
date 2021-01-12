2c6ae1b71f418e5bf8b2572d103a172d4dba4b15
Xuong Huynh <xuong.huynh@quodisys.com>
2021-01-12 18:20:19 +0700

    Update test form

--- angular-cli/qds-onlinetest/src/app/home/iq-test/iq-main/iq-main.component.ts
+++ angular-cli/qds-onlinetest/src/app/home/iq-test/iq-main/iq-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { CountdownComponent, CountdownConfig, CountdownEvent } from 'ngx-countdown';
+import { FormBuilder, FormGroup, Validators } from  '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,19 +10,72 @@ import { Router } from '@angular/router';
 })
 export class IqMainComponent implements OnInit {
 
+	IQTestForm: FormGroup;
 	config = {}
+	questions: any[] = [
+		{ 
+			id: '001', 
+			question: 'Which number should come next in the pattern?',
+			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+			active: true 
+		},
+		{ 
+			id: '002', 
+			question: 'Book is to Reading as Fork is to:',
+			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+		},
+		{ 
+			id: '003', 
+			question: 'Which of the following can be arranged into a 5-letter English word?',
+			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+		},
+		{ 
+			id: '004', 
+			question: 'What number best completes the analogy:',
+			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+		},
+		{ 
+			id: '005', 
+			question: 'What Does FAQ stand for?',
+			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+		},
+		{ 
+			id: '006', 
+			question: 'Which is the largest number?',
+			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+		},
+		{ 
+			id: '007', 
+			question: 'Which number has the smallest value?',
+			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+		},
+		{ 
+			id: '008', 
+			question: 'What will you get if you reduce 14/35 to the lowest term?',
+			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+		},
+		{ 
+			id: '009', 
+			question: 'Helpless and Legend have ____ meanings.',
+			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+		}
+	];
 
-	constructor(private elementRef: ElementRef, private router: Router) { }
+	constructor(private elementRef: ElementRef, private router: Router, private formBuilder: FormBuilder) { }
 
 	ngOnInit(): void {
 		this.config = {
-			leftTime: 10,
-			format: 'mm:ss'
+			leftTime: 1160,
+			format: 'mm : ss'
 		}
 	}
 	counterEvent(e: CountdownEvent) {
 		if(e.action == 'done') {
 			this.router.navigate(['/iq-test/result'])
 		}
+	}
+
+	onSubmit() {
+		console.log(this.IQTestForm.value);
 	}
 }
--- angular-cli/qds-onlinetest/src/app/home/iq-test/iq-test.module.ts
+++ angular-cli/qds-onlinetest/src/app/home/iq-test/iq-test.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IqTestRoutingModule } from './iq-test-routing.module';
 import { IqStartComponent } from "./iq-start/iq-start.component";
 import { IqResultComponent } from './iq-result/iq-result.component';
 import { IqMainComponent } from './iq-main/iq-main.component'
 import { CountdownModule } from 'ngx-countdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
 declarations: [
@@ -20,7 +21,9 @@ imports: [
     IqTestRoutingModule,
     RouterModule,
     FormsModule,
-    CountdownModule
+    CountdownModule,
+    ReactiveFormsModule,
+    TabsModule.forRoot()
 ]
 })
 export class IqTestModule { }
