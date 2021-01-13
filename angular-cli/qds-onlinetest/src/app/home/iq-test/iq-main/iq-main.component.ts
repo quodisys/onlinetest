9538010779f6b9104eeb8b347f7e937bebd29819
Xuong Huynh <xuong.huynh@quodisys.com>
2021-01-13 15:47:18 +0700

    IQ Test fixed

--- angular-cli/qds-onlinetest/src/app/home/iq-test/iq-main/iq-main.component.ts
+++ angular-cli/qds-onlinetest/src/app/home/iq-test/iq-main/iq-main.component.ts
@@ -20,29 +20,29 @@ export class IqMainComponent implements OnInit {
 	questions: any[] = [
 		{ 
 			id: '001', 
-			question: 'Which number should come next in the pattern?',
-			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+			question: 'What number is one quarter of one tenth of one fifth of 200?',
+			choices: ['25', '.5', '1', '10'],
 			active: true 
 		},
 		{ 
 			id: '002', 
-			question: 'Book is to Reading as Fork is to:',
-			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+			question: 'A palindrome is a word or phrase that is spelled the same written forward or backward, often used for I.Q. test questions. "Stets" is a palindrome.',
+			choices: ['True', 'False'],
 		},
 		{ 
 			id: '003', 
-			question: 'Which of the following can be arranged into a 5-letter English word?',
-			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+			question: 'Sequential reasoning is often tested in IQ exams. 3, 7, 13, 21, 31. What number comes next in the sequence?',
+			choices: ['37', '45', '43', '39', '49'],
 		},
 		{ 
 			id: '004', 
-			question: 'What number best completes the analogy:',
-			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+			question: 'Compare and contrast or classification problems are commonly used to measure intelligence. Which of the five is least like the other four?',
+			choices: ['Eel', 'Shark', 'Dolphin', 'Swordfish', 'Turtle'],
 		},
 		{ 
 			id: '005', 
-			question: 'What Does FAQ stand for?',
-			choices: ['Awnser A', 'Awnser B', 'Awnser C', 'Awnser D'],
+			question: 'If you rearrange the letters of "ahret," you would have the name of a:',
+			choices: ['Ventricle', 'Fish', 'River', 'Planet', 'Country'],
 		},
 		{ 
 			id: '006', 
@@ -77,6 +77,10 @@ export class IqMainComponent implements OnInit {
 				}
 			]
 		}
+		this.questions.map(item => {
+			item['customClass'] = '';
+			console.log(item)
+		})
 		this.config = {
 			leftTime: 1160,
 			format: 'mm : ss'
@@ -92,6 +96,7 @@ export class IqMainComponent implements OnInit {
 		let that = this;
 		let tabId = key + 1;
 		let questionCount = this.questions.length;
+		that.questions[key].customClass = 'done'
 		if(tabId < questionCount) {
 			setTimeout(function() {
 				that.staticTabs.tabs[tabId].active = true;
